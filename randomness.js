169e33f4d976137359e03a7ae1eeed755f04bc49
michaelnASC6 <mnewby0503@gmail.com>
2019-07-11 14:19:48 -0400

    Slight setup

---
+++ randomness.js
@@ -0,0 +1,32 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+function randomNumber(number){
+    console.log()
+}
+
+function randomLetter(){
+    console.log(alphabet)
+}
