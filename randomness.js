50b023cca2331a5d492f8b6d2baa2a1485114c1b
michaelnASC6 <mnewby0503@gmail.com>
2019-07-11 14:55:35 -0400

    Follow Pat - doesn't work

--- randomness.js
+++ randomness.js
@@ -23,14 +23,30 @@ const students = [
 // END OF GLOBAL VARIABLES //
 /////////////////////////////
 
-function randomNumber(number){
-    console.log()
+function randomNumber(){
+    console.log(random_number);
 }
 
-function randomLetter(){
-    console.log(alphabet)
-}
+// function randomLetter(){
+//     console.log(alphabet)
+// }
 
 
 const number = Math.random()
-const random_number = 21*number
+const random_number = 21 * number
+
+function randomLetter(){
+    const len = alphabet.length - 1;
+    const num = Math.random() * len;
+    const roundNum = math.floor(num);
+    const final = alphabet [roundNum]
+    // return alphabet [roundNum];
+ //   return final = alphabet [roundNum];
+    console.log("Len: ", Len);
+    //  return final;
+
+    // console.log ("num: " + num);
+
+}
+
+randomLetter();
